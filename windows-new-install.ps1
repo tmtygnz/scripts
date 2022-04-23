6d6e109c5da88982f611a20e021a5a66d2bc33bc
Timothy Ganoza <jostimian@gmail.com>
2022-04-23 21:18:36 +0800

    Init

---
+++ windows-new-install.ps1
@@ -0,0 +1,6 @@
+winget install Spotify.Spotify
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.WindowsTerminal.Preview
+winget install Discord.Discord
+winget install Node.js
+winget install Microsoft.VC++2022Redist-arm64
